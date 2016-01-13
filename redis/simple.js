6bc239aec64ebba35bfd5ab2c6019b8187705423
曹化宇 <huayu.cao@jimubox.com>
2016-01-13 16:40:18 +0800

    update

--- redis/simple.js
+++ redis/simple.js
@@ -1,20 +1,37 @@
-var redis = require("redis"),
-    client = redis.createClient();
- 
-// if you'd like to select database 3, instead of 0 (default), call 
-// client.select(3, function() { /* ... */ }); 
- 
+var redis = require("redis");
+var Promise = require("bluebird");
+
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+var client = redis.createClient({
+    port: 6379,
+    host: '127.0.0.1'
+});
+
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
 client.on("error", function (err) {
     console.log("Error " + err);
 });
- 
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
+var obj = {
+    name: 'rob',
+    age: 27,
+    color: 'black'
+};
+
+var keys = Object.keys(obj);
+
+for(var i = 0; i < keys.length; i++){
+
+    var key = keys[i];
+
+    client.hset("test", key, obj[key],redis.print);
+}
+
+client.hgetallAsync('test').then(function(res) {
+    console.log(res);
     client.quit();
 });
+
