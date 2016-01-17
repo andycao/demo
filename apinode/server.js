69272e31f04899bcf0114f30fcb18e3386840b07
Andy Cao <cao_huayu@hotmail.com>
2016-01-18 01:41:57 +0800

    seque orm

--- apinode/server.js
+++ apinode/server.js
@@ -1,7 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+//sequelize
+var Sequelize = require('sequelize');
+var seque = new Sequelize('test', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+
+var User = seque.define('test', {
+    username: Sequelize.STRING,
+    password: Sequelize.STRING
+});
+
+seque.sync().then(function() {
+  return User.create({
+    username: 'janedoe',
+    password: 'test'
+  });
+}).then(function(jane) {
+  console.log(jane.get({
+    plain: true
+  }));
+});
+
 var app = express();
+
 
 app.use(bodyParser.urlencoded({extended: true})); //support x-www-form-urlencoded
 app.use(bodyParser.json());
