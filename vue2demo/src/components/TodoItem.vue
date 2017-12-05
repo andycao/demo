a9596c04fe90cfa1a028b27c935f70bc06ad20d9
andycao <huayu.cao@pintec.com>
2017-12-05 19:22:32 +0800

    save

---
+++ vue2demo/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>{{todo.text}}</li>
+</template>
+
+<script>
+    export default {
+        props: {
+            todo: {
+                type: Object,
+                default: {}
+            }
+        },
+        name: 'todo-item'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue2demo/src/pages/App7.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app-7">
+        <ol>
+            <TodoItem
+                    v-for="item in groceryList"
+                    v-bind:todo="item"
+                    v-bind:key="item.id">
+            </TodoItem>
+        </ol>
+    </div>
+</template>
+
+<script>
+    import TodoItem from '@/components/TodoItem'
+
+    export default {
+        name: 'app7',
+        components: {TodoItem},
+        data () {
+            return {
+                groceryList: [
+                    {id: 0, text: '蔬菜'},
+                    {id: 1, text: '奶酪'},
+                    {id: 2, text: '随便其他什么人吃的东西'}
+                ]
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
