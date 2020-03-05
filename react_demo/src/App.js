e5f84cf936cbf3590667f09c09c729adebd84865
Andy Cao <cao_huayu@hotmail.com>
2020-03-06 00:09:24 +0800

    save

--- react_demo/src/App.js
+++ react_demo/src/App.js
@@ -1,47 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import DocumentTitle from 'react-document-title';
 
 function App() {
     const NewHeader = logProps(MyHeader);
     const [title, setTitle] = useState("默认的标题");
 
     return (
-        <div className="App">
 
-            <NewHeader title={title}></NewHeader>
-            <button onClick={()=>{
-                setTitle(`title ${Math.random()}`)
-            }}>ok</button>
-        </div>
+            <div className="App">
+                <NewHeader title={title}></NewHeader>
+                <button onClick={() => {
+                    setTitle(`title ${Math.random()}`)
+                }}>ok</button>
+            </div>
+
     );
 }
 
+class MyHeader extends React.Component {
 
-
-function MyHeader(props) {
-    return (
-        <div>
-            <h2>{props.title}</h2>
-            <hr />
-        </div>
-    );
+    componentDidMount(){
+        console.log('child did mount');
+    }
+    render(){
+        return (
+                <div>
+                <h2>{this.props.title}</h2>
+                <hr />
+            </div>
+        );
+    }
 }
 
 function logProps(WrappedComponent) {
     return class extends React.Component {
-      componentWillReceiveProps(nextProps) {
-        console.log('Current props: ', this.props);
-        console.log('Next props: ', nextProps);
-      }
-      render() {
-          const {title} = this.props;
-          console.log(title);
-          
-        // 将 input 组件包装在容器中，而不对其进行修改。Good!
-        return <WrappedComponent 
-        {...this.props} />;
-      }
+        componentDidMount(){
+            console.log('parent did mount');
+            console.log(this.props);
+        }
+        render() {
+            const { title } = this.props;
+            // 将 input 组件包装在容器中，而不对其进行修改。Good!
+            return <DocumentTitle title={title}>
+                <WrappedComponent{...this.props} />
+            </DocumentTitle>
+        }
     }
-  }
+}
 
 export default App;
